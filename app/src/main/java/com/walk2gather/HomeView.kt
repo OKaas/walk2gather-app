0fd796a4f3ed6c427771d866792df391c5fdcea9
kaas@kiv.zcu.cz <kaasondrej@gmail.com>
2018-10-15 22:48:48 +0200

    added toolbar in main activity

--- app/src/main/java/com/walk2gather/HomeView.kt
+++ app/src/main/java/com/walk2gather/HomeView.kt
@@ -2,11 +2,58 @@ package com.walk2gather
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 
 class HomeView : AppCompatActivity() {
 
+    // Const
+    // ===========================================================================================
+    private val TAG = this::class.java.simpleName
+
+
+    // Initializer
+    //
+    // ===========================================================================================
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_view)
+
+
+        // Get the Intent that started this activity and extract the string
+        val loginName = intent.getStringExtra(LoginActivity.LOGIN_NAME)
+        val loginPassword = intent.getStringExtra(LoginActivity.LOGIN_PASSWORD)
+
+        Log.i(TAG, "$loginName > $loginPassword" )
+    }
+
+    // Toolbar
+    //
+    // ===========================================================================================
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        item.isChecked = true
+
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.main_menu_action_to_join -> {
+                Log.i(TAG, "Clicked Add")
+                item.isChecked = false
+                true
+            }
+            R.id.main_menu_action_members -> {
+                Log.i(TAG, "Clicked setting")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
--- app/src/main/java/com/walk2gather/LoginActivity.kt
+++ app/src/main/java/com/walk2gather/LoginActivity.kt
@@ -1,23 +1,36 @@
 package com.walk2gather
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-
+import android.widget.EditText
 
 class LoginActivity : AppCompatActivity() {
 
     private val TAG = this::class.java.simpleName
 
+    companion object {
+        const val LOGIN_NAME = "loginActivity_name"
+        const val LOGIN_PASSWORD = "loginActivity_password"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_view)
     }
 
-    fun login(view: View ) {
-        Log.d(TAG,"Clicking!")
+    fun login(view: View) {
+        Log.d(TAG, "Clicking!")
+
+        val loginName = findViewById<EditText>(R.id.login_textView_name).text.toString()
+        val loginPassowrd = findViewById<EditText>(R.id.login_textView_password).text.toString()
+        val intent = Intent(this, HomeView::class.java).apply {
+            putExtra(LOGIN_NAME, loginName)
+            putExtra(LOGIN_PASSWORD, loginPassowrd)
+        }
+        startActivity(intent)
     }
 
 }
